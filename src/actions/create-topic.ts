6e2f072b2664e56703ea334c56e62f44e94d960e
Patrick Sheridan <patricksheridan@Patricks-MacBook-Pro.local>
2024-08-19 16:41:54 -0700

    Error handling added and logging error in form

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,26 @@
-'use server';
+"use server";
 
-export async function createTopic() {
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lower case letters without dashes or spaces",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(formData: FormData) {
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description')
+  })
+
+  if(!result.success) {
+    console.log(result.error.flatten().fieldErrors)
+  }
 
   //TODO: revalidate homepage after topic created
 }
