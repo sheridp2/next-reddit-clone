afac896d6b3fb64da91d9c5528b374fee3155e80
Patrick Sheridan <patricksheridan@Patricks-MacBook-Pro.local>
2024-08-20 15:09:01 -0700

    Form now submitting, adding new page topic and redirecting to topic page

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,12 @@
 "use server";
 
+import type { Topic } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
+import { auth } from "@/auth";
+import { db } from "@/db";
+import paths from "@/paths";
 
 const createTopicSchema = z.object({
   name: z
@@ -16,6 +22,7 @@ interface CreateTopicFormState {
   errors: {
     name?: string[];
     description?: string[];
+    _form?: string[];
   };
 }
 
@@ -30,13 +37,44 @@ export async function createTopic(
 
   if (!result.success) {
     return {
-      errors: result.error.flatten().fieldErrors
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed in to do this."],
+      },
+    };
+  }
+
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description
+      }
+    })
+  } catch (err:unknown) {
+    if( err instanceof Error){
+      return {
+        errors: {
+          _form: [err.message]
+        }
+      }
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong']
+        }
+      }
     }
   }
 
-  return {
-    errors: {}
-  };
+  revalidatePath('/')
+  redirect(paths.topicShow(topic.slug));
 
-  //TODO: revalidate homepage after topic created
 }
